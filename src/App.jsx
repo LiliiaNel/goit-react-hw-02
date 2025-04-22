a97bd5d54823c13dff11981ee1563eb6ac757b0e
Lili <Liliia_Nel@proton.me>
2025-04-23 00:02:16 +0200

    change path for components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './App.css'
 import Description from './components/description/Description';
 import Options from './components/options/Options';
-import Feedback from './components/Feedback/Feedback';
+import Feedback from './components/feedback/Feedback';
 import Notification from './components/Notification/Notification';
 
 function App() {
